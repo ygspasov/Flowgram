0f50aa04656afc45f3c3b8ee31307e37d5ea8ec8
Yavor Spassov <yavor@hph.io>
2023-09-12 21:30:59 +0300

    Adding the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import Navbar from "@/components/NavBar.vue";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="">
+    <div class="wrapper"><Navbar /></div>
   </header>
 
   <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main>
-    <div class="about">
+    <div class="about max-w-screen-xl mx-auto">
       <h1 class="text-3xl font-bold underline">Home</h1>
     </div>
   </main>

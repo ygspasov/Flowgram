9525683bd03d546e54870bab2c969e78e7917cb1
Yavor Spassov <yavor@hph.io>
2023-09-13 18:48:46 +0300

    Adding display logic for the Signup/Login forms

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -85,12 +85,13 @@
               class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
             >
               <li>
-                <a
+                <!-- <a
                   href="/"
                   class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white"
                   aria-current="page"
                   >Signup</a
-                >
+                > -->
+                <AuthModal :isLogin="false" />
               </li>
               <li>
                 <!-- <a
@@ -98,7 +99,7 @@
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >Login</a
                 > -->
-                <AuthModal />
+                <AuthModal :isLogin="true" />
               </li>
             </ul>
           </div>

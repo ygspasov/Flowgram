f6abd9e81e563c3433d322da5c127a7b7252f756
Yavor Spassov <ygspasov@gmail.com>
2023-09-13 04:50:30 +0300

    Adding the Login modal

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -93,11 +93,12 @@
                 >
               </li>
               <li>
-                <a
+                <!-- <a
                   href="/"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                   >Login</a
-                >
+                > -->
+                <AuthModal />
               </li>
             </ul>
           </div>
@@ -108,6 +109,7 @@
 </template>
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+import AuthModal from "./auth/AuthModal.vue";
 import { ref } from "vue";
 </script>
 <style></style>

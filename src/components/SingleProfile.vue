8ab66db3cc203c6089285251368d4a2fc135b872
Yavor Spassov <yavor@hph.io>
2023-09-15 16:25:07 +0300

    Adding the posts, followers and following sections

--- src/components/SingleProfile.vue
+++ src/components/SingleProfile.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="">
-    <div class="flex flex-wrap justify-between bg-slate-300 p-5 my-2">
-      <h2 class="text-2xl flex items-center">User name</h2>
-      <button
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        upload photo
-      </button>
+    <div class="bg-slate-300 my-2">
+      <div class="flex flex-wrap justify-between p-5">
+        <h2 class="text-2xl flex items-center">User name</h2>
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          upload photo
+        </button>
+      </div>
+      <div class="flex flex-col sm:flex-row pb-5">
+        <span class="w-100 sm:w-1/3 text-lg text-center">13212 posts</span>
+        <span class="w-100 sm:w-1/3 text-lg text-center">300 followers</span>
+        <span class="w-100 sm:w-1/3 text-lg text-center">4120 following</span>
+      </div>
     </div>
     <div class="flex flex-wrap justify-center bg-slate-300 my-5 p-5">
       <img

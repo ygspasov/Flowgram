9aef240f1f32bc6fe2493c693c5cc09f5789cd7c
Yavor Spassov <yavor@hph.io>
2023-09-22 13:52:16 +0300

    Adding the upload modal

--- src/components/profile/UserBar.vue
+++ src/components/profile/UserBar.vue
@@ -2,13 +2,14 @@
   <div class="bg-slate-300 my-2">
     <div class="flex flex-wrap justify-between p-5">
       <h2 class="text-2xl flex items-center">{{ props.username }}</h2>
-      <button
-        v-if="userLoggedIn && userCheck"
+      <!-- <button
+        v-if="userCheck"
         type="button"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
         upload photo
-      </button>
+      </button> -->
+      <UploadModal v-if="userCheck" />
     </div>
     <div class="flex flex-col sm:flex-row pb-5">
       <span class="w-100 sm:w-1/3 text-lg text-center">{{ userInfo.posts }} posts</span>
@@ -18,11 +19,13 @@
   </div>
 </template>
 <script setup lang="ts">
+import { initFlowbite } from "flowbite";
 import { type userInfo } from "@/types/UserInfo";
 import { authStore } from "@/stores/auth";
 import { storeToRefs } from "pinia";
 import { useRoute } from "vue-router";
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
+import UploadModal from "./UploadModal.vue";
 const store = authStore();
 const { userLoggedIn }: any = storeToRefs(store);
 const props = defineProps<{
@@ -34,6 +37,9 @@ const { username: profileUsername } = route.params;
 let { username } = storeToRefs(store);
 const authUsername: string = username.value.toLowerCase();
 //Checking if the user profile belongs to the user in order to enable the upload
-const userCheck = computed(() => profileUsername === authUsername);
+const userCheck = computed(() => userLoggedIn && profileUsername === authUsername);
+onMounted(() => {
+  initFlowbite();
+});
 </script>
 <style></style>

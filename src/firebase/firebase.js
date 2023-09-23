7ef5ad0217ca6a9105bc50ce3f2320e39a29fb95
Yavor Spassov <ygspasov@gmail.com>
2023-09-23 23:03:46 +0300

    Uploading a picture to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 
@@ -21,3 +22,6 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };

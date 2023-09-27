c7d90611086e3cbc9ce0721a1f766aec17c2c2f5
Yavor Spassov <ygspasov@gmail.com>
2023-09-28 01:26:35 +0300

    Persisting the user ID in localstorage

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -49,6 +49,7 @@ export const authStore = defineStore("auth", {
           console.log("user", user);
           this.setUser(user);
           this.userLoggedIn = true;
+          localStorage.setItem("uid", user.uid);
         })
         .catch((error) => {
           const errorCode = error.code;

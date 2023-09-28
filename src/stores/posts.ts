9ce0d5872f5cda7c0b3ea843163262712b31ce68
Yavor Spassov <yavor@hph.io>
2023-09-29 00:32:52 +0300

    Fixing the profile button's routing

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,10 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-// @ts-ignore
-import { db } from "@/firebase/firebase.js";
-import { collection, query, where, getDocs } from "firebase/firestore";
-
 export const postsStore = defineStore("posts", {
   state: () => ({
     loadPosts: ref(false),

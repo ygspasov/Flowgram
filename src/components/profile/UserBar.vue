740997c562e7845019b5d5dd3ac550ba54551806
Yavor Spassov <ygspasov@gmail.com>
2023-09-30 01:08:26 +0300

    Adding the Follow button to the user's profile

--- src/components/profile/UserBar.vue
+++ src/components/profile/UserBar.vue
@@ -11,7 +11,15 @@
       >
         upload photo
       </button> -->
-      <UploadModal v-if="userCheck" />
+      <div id="buttonGroup" class="flex flex-col">
+        <UploadModal v-if="userCheck" />
+        <button
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          type="button"
+        >
+          Follow
+        </button>
+      </div>
     </div>
     <div class="flex flex-col sm:flex-row pb-5">
       <span class="w-100 sm:w-1/3 text-lg text-center">{{ userInfo.posts }} posts</span>

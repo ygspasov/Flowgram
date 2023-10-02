630e381c69da95623b2d6b75e7528d9acc0cd209
Yavor Spassov <ygspasov@gmail.com>
2023-10-02 03:39:25 +0300

    Only rendering the user profile for existing users

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,9 +1,43 @@
 <template>
-  <div class="my-5">
-    <SingleProfile />
+  <div class="my-5 text-lg">
+    <SingleProfile v-if="userExists" />
+
+    <div v-else>
+      <p class="text-xl text-red-600 text-center">The user doesn't exist.</p>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+import { watch, ref, onMounted } from "vue";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase/firebase.js";
 import SingleProfile from "@/components/profile/SingleProfile.vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+let username = route.params.username;
+const userExists = ref(true);
+watch(route, () => {
+  //watching for changes in the router parameter(username) and updating the profile images
+  console.log("watcher running");
+  username = route.params.username;
+  console.log("username from router", username);
+  checkUsernameExists(username);
+});
+
+const checkUsernameExists = async (usernameToCheck) => {
+  // Convert the username to lowercase (or uppercase) to ignore case
+  const lowerUsername = usernameToCheck.toLowerCase();
+
+  // Get the document reference for the provided username
+  const profileUIDRef = doc(db, "usernameToUID", lowerUsername);
+
+  try {
+    // Get the document data
+    const docSnap = await getDoc(profileUIDRef);
+    userExists.value = docSnap.exists();
+  } catch (error) {
+    console.error("Error checking username:", error);
+    throw error;
+  }
+};
 </script>
-<style></style>

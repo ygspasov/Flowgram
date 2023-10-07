4ba81b3c48281066d610a3a0634787451d1b5fa5
Yavor Spassov <ygspasov@gmail.com>
2023-10-07 05:31:52 +0300

    Restructuring the timeline and only showing posts when user is authenticated

---
+++ src/components/timeline/UserTimeline.vue
@@ -0,0 +1,13 @@
+<template>
+  <div v-if="userLoggedIn"><Cards /></div>
+  <div v-else><h3 class="text-lg text-center">Log in to see posts.</h3></div>
+</template>
+<script setup lang="ts">
+import Cards from "./Cards.vue";
+
+import { authStore } from "@/stores/auth";
+import { storeToRefs } from "pinia";
+const auth_Store = authStore();
+const { userLoggedIn } = storeToRefs(auth_Store);
+</script>
+<style></style>

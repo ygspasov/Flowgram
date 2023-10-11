cd01e1bd4ad105b738c7b72d10bb0221be20af06
Yavor Spassov <ygspasov@gmail.com>
2023-10-11 15:03:55 +0300

    Reusing the timeline card component in the user profile

--- src/components/profile/UserGallery.vue
+++ src/components/profile/UserGallery.vue
@@ -5,30 +5,7 @@
       class="flex flex-wrap flex-col md:flex-row justify-between bg-slate-300 my-5 p-5"
     >
       <div class="mb-5 w-full md:w-1/3" v-for="post in posts" :key="post.id">
-        <div
-          class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-        >
-          <!-- <img class="w-full max-w-sm" :src="post.downloadURL" alt="image description" /> -->
-          <a :href="post.downloadURL" class="flex justify-center" target="_blank">
-            <img class="rounded-t-lg" :src="post.downloadURL" alt="" />
-          </a>
-          <div class="p-5">
-            <a href="#">
-              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {{ post.description }}
-              </h5>
-            </a>
-            <!-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {{ timeFormat }}
-      </p> -->
-            <button
-              type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-              Like
-            </button>
-          </div>
-        </div>
+        <SingleCard :post="post" />
       </div>
     </div>
     <div v-else>
@@ -43,6 +20,7 @@ import { type Post } from "@/types/Post";
 import { storeToRefs } from "pinia";
 import { authStore } from "@/stores/auth";
 import { useRoute } from "vue-router";
+import SingleCard from "@/components/timeline/SingleCard.vue";
 const route = useRoute();
 const store = authStore();
 const { userLoggedIn }: any = storeToRefs(store);
@@ -66,7 +44,6 @@ watch(
 );
 
 const { profilePosts: posts } = toRefs(props);
-
 onMounted(() => {
   console.log("posts", posts);
 });
--- src/components/timeline/SingleCard.vue
+++ src/components/timeline/SingleCard.vue
@@ -7,7 +7,7 @@
     </a>
     <div class="p-5">
       <a href="#">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
           {{ post.description }}
         </h5>
       </a>

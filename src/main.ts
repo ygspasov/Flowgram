55287bf1d43d8b99a12d3e4defe5d42861a9dffb
Yavor Spassov <yavor@hph.io>
2023-10-13 19:25:30 +0300

    Installing fontawesome icons and adding them to SingeCard

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,19 @@ import "./firebase/firebase";
 import "./index.css";
 import "flowbite";
 
-const app = createApp(App);
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faPenToSquare, faTrash, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faPenToSquare, faTrash, faThumbsUp);
+
+const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(createPinia());
 app.use(router);

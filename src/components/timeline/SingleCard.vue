b436508cb212220913a6c8cd02c6180f28cf7ce4
Yavor Spassov <yavor@hph.io>
2023-10-13 13:28:49 +0300

    Adding info on who posted a post on each card

--- src/components/timeline/SingleCard.vue
+++ src/components/timeline/SingleCard.vue
@@ -11,9 +11,8 @@
           {{ post.description }}
         </h5>
       </a>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-        {{ timeFormat }}
-      </p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Posted by {{ post.username }}</p>
+      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">On {{ timeFormat }}</p>
       <button
         type="button"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
--- src/types/Post.ts
+++ src/types/Post.ts
@@ -7,4 +7,5 @@ export type Post = {
   type: string;
   uid: string;
   description: string;
+  username: string;
 };

313a9a925774260880e750ccaee8ba9b0207d48a
Yavor Spassov <yavor@hph.io>
2023-10-20 13:18:08 +0300

    Adding post deletion to user timeline

--- src/components/timeline/Cards.vue
+++ src/components/timeline/Cards.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-wrap justify-around items-start my-5">
-    <div v-for="post in morePosts">
-      <SingleCard :post="post" />
+    <div v-for="post in morePosts" :key="post.id">
+      <SingleCard :post="post" @deletePostId="deletePostId" />
     </div>
   </div>
 </template>
@@ -10,6 +10,14 @@ import SingleCard from "./SingleCard.vue";
 import { postsStore } from "@/stores/posts";
 import { storeToRefs } from "pinia";
 const posts_Store = postsStore();
-let { morePosts } = storeToRefs(posts_Store);
+let { morePosts, timelinePosts } = storeToRefs(posts_Store);
+const deletePostId = (id: string) => {
+  const objWithIdIndex = morePosts.value.findIndex((post) => post.id === id);
+  if (objWithIdIndex > -1) {
+    // console.log("post to be deleted", morePosts.value[objWithIdIndex]);
+    // console.log("morePosts.value", morePosts.value);
+    morePosts.value.splice(objWithIdIndex, 1);
+    timelinePosts.value.splice(objWithIdIndex, 1);
+  }
+};
 </script>
-<style></style>

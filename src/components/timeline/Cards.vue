075b0870d1164fd42cdc59f713a907b3362251a5
Yavor Spassov <ygspasov@gmail.com>
2023-10-21 17:04:29 +0300

    Updating the UI when a post is deleted

--- src/components/timeline/Cards.vue
+++ src/components/timeline/Cards.vue
@@ -10,14 +10,22 @@ import SingleCard from "./SingleCard.vue";
 import { postsStore } from "@/stores/posts";
 import { storeToRefs } from "pinia";
 const posts_Store = postsStore();
-let { morePosts, timelinePosts } = storeToRefs(posts_Store);
+let { morePosts, profilePosts } = storeToRefs(posts_Store);
 const deletePostId = (id: string) => {
   const objWithIdIndex = morePosts.value.findIndex((post) => post.id === id);
   if (objWithIdIndex > -1) {
     // console.log("post to be deleted", morePosts.value[objWithIdIndex]);
     // console.log("morePosts.value", morePosts.value);
     morePosts.value.splice(objWithIdIndex, 1);
-    timelinePosts.value.splice(objWithIdIndex, 1);
+  }
+  deleteProfilePostId(id);
+};
+const deleteProfilePostId = (id: string) => {
+  const objWithIdIndex = profilePosts.value.findIndex((post) => post.id === id);
+  if (objWithIdIndex > -1) {
+    // console.log("profile post to be deleted", profilePosts.value[objWithIdIndex]);
+    profilePosts.value.splice(objWithIdIndex, 1);
+    // posts_Store.setPostsLoading(true);
   }
 };
 </script>

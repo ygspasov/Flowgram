b2cd56fc9fad03b6ef450ae70c76af0f60c45faf
Yavor Spassov <ygspasov@gmail.com>
2023-10-23 15:49:16 +0300

    Adding loading indicators for profile and timeline posts

--- src/components/profile/SingleProfile.vue
+++ src/components/profile/SingleProfile.vue
@@ -6,8 +6,22 @@
       :key="$route.params.username"
       @followAction="followAction"
     />
-    <userGallery v-if="store.profilePosts.length > 0" :profilePosts="profilePosts" />
-    <div v-else><p>The user hasn't uploaded any content yet.</p></div>
+    <div class="flex items-center justify-center">
+      <div
+        v-if="loading"
+        class="flex items-center justify-center w-56 h-16 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div
+          class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200"
+        >
+          Loading posts...
+        </div>
+      </div>
+    </div>
+    <userGallery v-if="store.profilePosts.length && !loading" :profilePosts="profilePosts" />
+    <div v-else class="flex items-center justify-center">
+      <p>The user hasn't uploaded any content yet.</p>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -24,6 +38,7 @@ import { useRoute } from "vue-router";
 const route = useRoute();
 const componentKey = ref(route.fullPath);
 const store = postsStore();
+const loading = ref(false);
 watch(route, () => {
   //watching for changes in the router parameter(username) and updating the profile profilePosts
   componentKey.value = route.fullPath;
@@ -46,6 +61,7 @@ let docSnap: any;
 let profileUID: any;
 
 const getProfileUID = async () => {
+  loading.value = true;
   store.setClearProfilePosts();
   const username = route.params.username;
   console.log("username", username);
@@ -64,6 +80,11 @@ const getProfileUID = async () => {
     })
     .then(() => {
       getPosts();
+    })
+    .then(() => {
+      setTimeout(() => {
+        loading.value = false;
+      }, 2000);
     })
     .catch((error) => {
       console.error("Error getting document:", error);
--- src/components/timeline/UserTimeline.vue
+++ src/components/timeline/UserTimeline.vue
@@ -1,5 +1,19 @@
 <template>
-  <div v-if="userLoggedIn"><Cards :userIDs="userIDs" /></div>
+  <div v-if="userLoggedIn">
+    <div class="flex items-center justify-center mt-3">
+      <div
+        v-if="loading"
+        class="flex items-center justify-center w-56 h-16 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div
+          class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200"
+        >
+          Loading posts...
+        </div>
+      </div>
+    </div>
+    <Cards :userIDs="userIDs" />
+  </div>
   <div v-else><h3 class="text-lg text-center">Log in to see posts.</h3></div>
   <PostsObserver v-if="delay" @intersect="fetchMorePosts" />
 </template>
@@ -18,6 +32,7 @@ let userIDs = ref(following.value);
 let delay = ref(false);
 let firstCardIndex = ref(0);
 let lastCardIndex = ref(6);
+let loading = ref(false);
 
 watch(userLoggedIn, (newVal) => {
   console.log("userUID.value", userUID.value);
@@ -42,12 +57,18 @@ const getTimelinePosts = () => {
     .then(() => {
       console.log("timeline following", following.value);
       posts_Store.setTimelinePosts(userIDs.value);
+      loading.value = true;
     })
     .then(() => {
       //Introduces a delay so that fetchMorePosts doesn't run when the component is loaded
       setTimeout(() => {
         delay.value = true;
       }, 1000);
+    })
+    .then(() => {
+      setTimeout(() => {
+        loading.value = false;
+      }, 2000);
     });
 };
 
